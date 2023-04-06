8888124f04082691ab41d50019a858c9b8cf12f0
Belzedaar <6566875+belzedaar@users.noreply.github.com>
2023-04-06 16:29:39 +1000

    Pass Entry - log host

--- __init__.py
+++ __init__.py
@@ -10,16 +10,30 @@ from homeassistant.components import conversation
 
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Initialize your integration."""
 
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    conversation.async_set_agent(hass, entry, RhasspyConversationAgent())
+    conversation.async_set_agent(hass, entry, RhasspyConversationAgent(hass, entry))
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload Rhasspy Conversation."""
+    conversation.async_unset_agent(hass, entry)
     return True
 
 class RhasspyConversationAgent(agent.AbstractConversationAgent):
+
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
+        """Initialize the agent."""
+        self.hass = hass
+        self.entry = entry
+        self.host = entry.data["host"]
+        _LOGGER.info(f"Host: {self.host}")
 
     @property
     def attribution(self) -> agent.Attribution:
